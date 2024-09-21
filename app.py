c9e945147a4eda281b5afdaadd05e76959358181
Nelo-cmd <107011670+Nelo-cmd@users.noreply.github.com>
2024-09-21 03:49:27 +0100

    Just checking something

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 import os
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config["SESSION_PERMANENT"] = False
-app.config['SESSION_COOKIE_SECURE'] = False
+app.config['SESSION_COOKIE_SECURE'] = True
 app.config['WTF_CSRF_SSL_STRICT'] = False
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)
 app.config["SESSION_TYPE"] = "filesystem"
